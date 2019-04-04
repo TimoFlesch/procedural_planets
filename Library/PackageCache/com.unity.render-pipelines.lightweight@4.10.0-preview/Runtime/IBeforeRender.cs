3ce96a53f22dfe1fc365bcd2079ae8ba8b0f32d5
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2019-04-04 22:16:50 +0100

    new gitignore

--- Library/PackageCache/com.unity.render-pipelines.lightweight@4.10.0-preview/Runtime/IBeforeRender.cs
+++
@@ -1,8 +0,0 @@
-namespace UnityEngine.Experimental.Rendering.LightweightPipeline
-{
-    public interface IBeforeRender
-    {
-        ScriptableRenderPass GetPassToEnqueue(RenderTextureDescriptor baseDescriptor,
-            RenderTargetHandle colorHandle, RenderTargetHandle depthHandle, ClearFlag clearFlag);
-    }
-}
